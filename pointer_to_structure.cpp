0ab244ac52ef39682d6e3510ab152120a148d945
Snehal Banerjee <144614232+Snehal3704@users.noreply.github.com>
2024-05-09 23:06:58 +0530

    Add files via upload

---
+++ pointer_to_structure.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //cout<<"hello world\n"<< "hello\n" << "hey";
+    int i=10;
+
+    int a=i++;
+    cout<<i;
+    cout<<" ";
+
+    cout<<a;
+}
