4a999469fb8640707232f0d171c5449b453fd3c1
Snehal Banerjee <144614232+Snehal3704@users.noreply.github.com>
2024-05-17 00:47:02 +0530

    Update permutation_tech2.cpp

--- permutation_tech2.cpp
+++ permutation_tech2.cpp
@@ -1,19 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void getpermutations(vector<vector<int>> &res, vector<int> &temp, vector<int> &arr, int size, int ind) {
-    if (ind == size) {
-        res.push_back(temp);
+void getpermutations( vector<int> &arr, int size, int ind) {
+
+    if(ind == size){
+        cout<<"[";
+
+        for(auto it : arr){
+            cout<<it;
+        }
+        cout<<"]";
+        cout<<endl;
+        
         return;
     }
 
-    for (int i = ind; i < size; i++) {
-        swap(arr[ind], arr[i]);
-        temp.push_back(arr[ind]);
-        getpermutations(res, temp, arr, size, ind + 1);
-        temp.pop_back();
-        swap(arr[ind], arr[i]);
+    for(int i = ind ; i< size ; i++){
+        swap(arr[ind],arr[i]);
+        
+        getpermutations(arr,size, ind + 1);
+        swap(arr[ind],arr[i]);
+
     }
+    
+
+
 }
 
 int main() {
@@ -28,17 +39,11 @@ int main() {
         cin >> arr[i];
     }
 
-    vector<vector<int>> res;
+    
     vector<int> temp;
 
-    getpermutations(res, temp, arr, size, 0);
+    getpermutations(arr, size, 0);
 
-    for (int i = 0; i < res.size(); i++) {
-        for (int j = 0; j < res[i].size(); j++) {
-            cout << res[i][j] << " ";
-        }
-        cout << endl;
-    }
 
     return 0;
 }
