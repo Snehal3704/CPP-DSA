40e44cdf332bf0a957cb2ec68a49947ad85dc1c1
Snehal Banerjee <144614232+Snehal3704@users.noreply.github.com>
2024-08-04 23:16:54 +0530

    Update LeadersIn_Arrays.cpp

--- LeadersIn_Arrays.cpp
+++ LeadersIn_Arrays.cpp
@@ -28,13 +28,14 @@ vector<int> brute(vector<int> arr,int n){
 
     vector<int> ans;
     int maxi =INT_MIN;
+    //O(n)
     for(int i=n-1 ; i>=0; i--){
         if(arr[i] > maxi){
             ans.push_back(arr[i]);
         }
         maxi = max(arr[i] , maxi);
     }
-
+    //O(nlogn)
     sort(ans.begin(),ans.end());
 
     return ans;
