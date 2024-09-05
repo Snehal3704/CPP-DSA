4fa6e81e17396c347d7d715bcbac7ef62ac92ffa
Snehal Banerjee <144614232+Snehal3704@users.noreply.github.com>
2024-09-05 22:39:54 +0530

    Update 4sum.cpp

--- 4sum.cpp
+++ 4sum.cpp
@@ -8,6 +8,7 @@ using namespace std;
 vector<vector<int>> brute(vector<int> arr , int n , int target){
     set<vector<int>> st;
 
+    
     for(int i =0 ; i< n ; i++){
         for(int j = i + 1 ; j < n ; j++){
             for(int k = j +1 ; k < n ; k++){
