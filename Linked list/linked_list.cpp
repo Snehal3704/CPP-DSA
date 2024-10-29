f97683a41691ea5ca6a986e962b9a7c8d544935e
Snehal Banerjee <144614232+Snehal3704@users.noreply.github.com>
2024-10-29 23:26:35 +0530

    Add files via upload

---
+++ Linked list/linked_list.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class node{
+    public:
+
+    int data;
+    node* nextptr;
+
+    
+    node(int data1){
+        data =data1;
+        nextptr = NULL;
+    }
+
+
+
+};
+
+node* convert_arr_to_ll(vector<int> arr){
+
+    node * head = new node(arr[0]);
+    node* mover = head;
+
+    for(int i=1 ; i< arr.size() ; i++){
+
+        node* temp = new node(arr[i]);
+        mover->nextptr = temp;
+        mover = temp;
+    }
+
+    return head;
+
+
+}
+
+int length(node*head){
+    
+    int cnt =1;
+    node* temp = head;
+
+    while (temp != NULL){
+        
+        temp=temp->nextptr;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+int main(){
+
+    
+    vector<int> arr = {5,3,4,7,6,2};
+    node* head = convert_arr_to_ll(arr);
+
+    cout<< length(head);
+
+    //traversaL
+    // node* temp = head;
+
+    // while (temp != NULL){
+    //     cout<< temp->data << " ";
+    //     temp=temp->nextptr;
+    // }
+
+
+
+    return 0;
+
+}
+
